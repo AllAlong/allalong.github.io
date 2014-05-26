7ddd496d65abcee88b17de7ae1347cba8eb86535
Thomas Sloboda <thomas@sloboda.fr>
2014-05-26 16:02:36 +0200

    fix (core) : Mise a jour routes
    Mise a jour des route et du design du menu suivant la route courrante

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,30 +1,25 @@
 angular.module(app.id).controller('mainController', ['$location', '$scope', '$rootScope', function($location, $scope, $rootScope) {
 
   $scope.homeActive = ($location.path() === '/');
-  $scope.aboutActive = ($location.path() === '/about');
-  $scope.teamActive = ($location.path() === '/team');
+  $scope.membersActive = ($location.path() === '/members');
+  $scope.battlesActive = ($location.path() === '/battles');
   $scope.utilsActive = ($location.path() === '/utils');
 
   $rootScope.$on('$routeChangeSuccess', function(event, current, previous, rejection) {
     $scope.homeActive = false;
-    $scope.aboutActive = false;
-    $scope.teamActive = false;
-    $scope.contactActive = false;
+    $scope.membersActive = false;
+    $scope.battlesActive = false;
     if ($location.path() === '/') {
       $scope.homeActive = true;
       document.title = app.name + ' - Home';
     }
-    if ($location.path() === '/about') {
-      $scope.aboutActive = true;
-      document.title = app.name + ' - About';
-    }
-    if ($location.path() === '/team') {
-      $scope.teamActive = true;
+    if ($location.path() === '/members') {
+      $scope.membersActive = true;
       document.title = app.name + ' - Members';
     }
-    if ($location.path() === '/utils') {
-      $scope.utilsActive = true;
-      document.title = app.name + ' - Utils';
+    if ($location.path() === '/battles') {
+      $scope.battlesActive = true;
+      document.title = app.name + ' - Battles';
     }
     if ($location.path() === '/404') {
       document.title = app.name + ' - Not Found';
